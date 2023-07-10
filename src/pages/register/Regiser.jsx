ec624e63436d80e4dc71547eebd530b6a3bb593e
muizu <muizu0920555@protonmail.com>
2023-07-10 19:31:31 +0900

    register

--- src/pages/register/Regiser.jsx
+++ src/pages/register/Regiser.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import "./Register.css"
+import "./Register.css";
 
 export default function Register(){
-    return <div className="register"></div>
+    return  (
+    <div className="register">
+        <div className="registerWrapper">
+            <div className="registerLeft">
+                <h3 className="registerLogo">YouTube WatchTime Tracker</h3>
+                <span className="registerDesc">Preventing Excessive Viewing</span>
+            </div>
+            <div className="registerRight">kdfagkjajgapgj</div>
+        </div>
+    </div>
+    );
 }
