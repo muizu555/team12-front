71a7da23203b7235afce288a52a09cde696d9237
muizu <muizu0920555@protonmail.com>
2023-07-10 23:36:25 +0900

    homeページの基本設定

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import Register from './pages/register/Regiser';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return <Register></Register>
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div>ここにランキングページのhtmlを書いてほしいです。そしてHome.cssにcssを書いてください</div>
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
