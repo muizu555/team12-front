9d87e8afd39f5290418d60d415f9afa050c73312
muizu <muizu0920555@protonmail.com>
2023-07-10 23:50:54 +0900

    routing diff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
+import Home from './pages/home/Home';
 import Register from './pages/register/Regiser';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
+
+//もしかしたら大変な間違いを犯してしまったかもしれません　ホームのパスが/:idなのかもしれない　このアプリでは、("/")というpathは存在しないのかもしれません
+
+
 function App() {
-  return <Register></Register>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Home />}/>
+        <Route path="/register" element = {<Register />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
