d79e18c8f266f85f18b00159af6a81552daf5953
muizu <muizu0920555@protonmail.com>
2023-07-11 15:43:27 +0900

    i

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //もしかしたら大変な間違いを犯してしまったかもしれません　ホームのパスが/:idなのかもしれない　このアプリでは、("/")というpathは存在しないのかもしれません
 
-
+ //多分userIdを返せばいけるはずです//
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element = {<Home />}/>
+        <Route path="/" element = {<Home />}/> 
         <Route path="/register" element = {<Register />}/>
       </Routes>
     </Router>
   );
 }
+
+
+
 
 export default App;
