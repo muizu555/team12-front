afb348a03ede4bc845481bd9ffb15372ddaa145f
muizu <muizu0920555@protonmail.com>
2023-07-14 17:40:32 +0900

    good

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -6,6 +6,7 @@ import { useGoogleLogin } from "@react-oauth/google";
 export default function GoogleLogin() {
     const googleLogin = useGoogleLogin({
         flow: 'auth-code',
+        scope: "https://www.googleapis.com/auth/youtube https://www.gdata.youtube.com https://gdata.youtube.com/ https://gdata.youtube.com http://gdata.youtube.com http://gdata.youtube.com/ https://gdata.youtube.com/feeds/ http://gdata.youtube.com/feeds/api/videos/ http://gdata.youtube.com/feeds/api/users/default/playlists http://gdata.youtube.com/youtube https://gdata.youtube.com/feeds/api/users/default/favorites/ https://gdata.youtube.com/feeds/api https://gdata.youtube.com/captions https://gdata.youtube.com/feed https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube.force-ssl",
         onSuccess: async (codeResponse) => {
             console.log(codeResponse);
             await axios.post('http://localhost:8000/api/v1/auth/googleauth',{code: codeResponse.code}, {
