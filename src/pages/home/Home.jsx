83ab67f79a84b27e97612fc64a641e15540950c9
noharu36 <132886617+noharu36@users.noreply.github.com>
2023-07-14 18:01:09 +0900

    フロントの見た目だけ

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,29 @@ export default function Home() {
     return (
       <>
         <Topbar />
+
+        <header id="header">
+          <a href="/">Youtube WatchTime Tracker</a>
+        </header>
+        <div className="body">
+          <div className="ranking">
+            <div id="title">
+              <h1>ranking</h1>
+            </div>
+            <div id="contents">
+              <Content />
+              <Content />
+              <Content />
+              <Content />
+              <Content />
+            </div>
+          </div>
+          <div className="profile">
+            <div id="myName"><h1>piyo</h1></div>
+            <div id="myWatchTime"><h1>01:23</h1></div>
+            <div id="myRanking"><h1>1位</h1></div>
+          </div>
+        </div>
       </>
     );
 
@@ -21,4 +44,16 @@ export default function Home() {
 
 
 
+}
+
+function Content() {
+  return (
+      <>
+          <div className="content">
+              <div className="order"><h1>1</h1></div>
+              <div className="userName"><h1>name</h1></div>
+              <div className="watchTime"><h1>01:23</h1></div>
+          </div>
+      </>
+  )
 }
