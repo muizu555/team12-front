b700bcdca45645b8f2ac2cb5e076afac1f4b5977
muizu <muizu0920555@protonmail.com>
2023-07-16 01:56:58 +0900

    deploy2

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -8,7 +8,8 @@ export default function GoogleLogin() {
     const [loggedInSuccess, setLoggedInSuccess] = useState(false)
 
     const googleLogin = useGoogleLogin({
-        flow: 'offline',
+        flow: 'auth-code',
+        ux_mode: 'popup',
         scope: "https://www.googleapis.com/auth/youtube https://www.gdata.youtube.com https://gdata.youtube.com/ https://gdata.youtube.com http://gdata.youtube.com http://gdata.youtube.com/ https://gdata.youtube.com/feeds/ http://gdata.youtube.com/feeds/api/videos/ http://gdata.youtube.com/feeds/api/users/default/playlists http://gdata.youtube.com/youtube https://gdata.youtube.com/feeds/api/users/default/favorites/ https://gdata.youtube.com/feeds/api https://gdata.youtube.com/captions https://gdata.youtube.com/feed https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube.force-ssl",
         onSuccess: async (codeResponse) => {
             console.log(codeResponse);
