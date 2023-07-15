b8a0d30e2aca409a315e40549c520850fb96af57
muizu <muizu0920555@protonmail.com>
2023-07-16 04:40:49 +0900

    deploy9

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -9,8 +9,8 @@ export default function GoogleLogin() {
 
     const googleLogin = useGoogleLogin({
         flow: 'auth-code',
-        ux_mode: 'redirect',
-        redirect_uri: 'https://team12-front.vercel.app/login',
+        ux_mode: 'popup',
+        // redirect_uri: 'https://team12-front.vercel.app/login',
         scope: "https://www.googleapis.com/auth/youtube https://www.gdata.youtube.com https://gdata.youtube.com/ https://gdata.youtube.com http://gdata.youtube.com http://gdata.youtube.com/ https://gdata.youtube.com/feeds/ http://gdata.youtube.com/feeds/api/videos/ http://gdata.youtube.com/feeds/api/users/default/playlists http://gdata.youtube.com/youtube https://gdata.youtube.com/feeds/api/users/default/favorites/ https://gdata.youtube.com/feeds/api https://gdata.youtube.com/captions https://gdata.youtube.com/feed https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube.force-ssl",
         onSuccess: async (codeResponse) => {
             console.log(codeResponse);
