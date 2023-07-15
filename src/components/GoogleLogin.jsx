e8759955e221c96368c50146b5574d0f0551cb77
noharu36 <132886617+noharu36@users.noreply.github.com>
2023-07-15 20:13:32 +0900

    ログイン完了時に/に遷移

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
-import React from 'react'
+import React, { useState } from 'react'
 import { useGoogleLogin } from "@react-oauth/google";
+import { Navigate } from "react-router-dom";
 
 
 export default function GoogleLogin() {
+    const [loggedInSuccess, setLoggedInSuccess] = useState(false)
+
     const googleLogin = useGoogleLogin({
         flow: 'auth-code',
         scope: "https://www.googleapis.com/auth/youtube https://www.gdata.youtube.com https://gdata.youtube.com/ https://gdata.youtube.com http://gdata.youtube.com http://gdata.youtube.com/ https://gdata.youtube.com/feeds/ http://gdata.youtube.com/feeds/api/videos/ http://gdata.youtube.com/feeds/api/users/default/playlists http://gdata.youtube.com/youtube https://gdata.youtube.com/feeds/api/users/default/favorites/ https://gdata.youtube.com/feeds/api https://gdata.youtube.com/captions https://gdata.youtube.com/feed https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube.force-ssl",
@@ -12,14 +15,20 @@ export default function GoogleLogin() {
             await axios.post('http://localhost:8000/api/v1/auth/googleauth',{code: codeResponse.code}, {
                 withCredentials: true,
             });
+            setLoggedInSuccess(true)
             // console.log(tokens);
         },
         onError: errorResponse => console.log(errorResponse),
     });
-  return (
-    <button onClick={googleLogin}>
-        Google Login
-    </button>
-  )
+
+    if (loggedInSuccess) {
+        <Navigate to="/" />
+    }
+
+    return (
+        <button onClick={googleLogin}>
+            Google Login
+        </button>
+    )
 }
 
